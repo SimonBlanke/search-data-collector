a1fc57bb23e173300e226c0fca2fb7ff8e8b015b
Simon Blanke <simon.blanke@yahoo.com>
2021-07-14 16:36:11 +0200

    add cleanup of files during testing

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -20,6 +20,9 @@ def search_data_equal(search_data1, search_data2):
     scores1 = search_data1["score"].values
     scores2 = search_data2["score"].values
 
+    print("\n scores1 \n", scores1, scores1.shape)
+    print("\n scores2 \n", scores2, scores2.shape)
+
     assert abs(np.sum(scores1) - np.sum(scores2)) < 0.001
 
 
@@ -27,13 +30,14 @@ def test_csv_0():
     collector = CsvDataCollector("./search_data.csv")
 
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function, search_space, n_iter=100)
+    hyper.add_search(objective_function, search_space, n_iter=100, memory=False)
     hyper.run()
 
     search_data1 = hyper.results(objective_function)
 
     collector.save(search_data1)
     search_data2 = collector.load()
+    collector.remove()
 
     search_data_equal(search_data1, search_data2)
 
@@ -42,13 +46,16 @@ def test_csv_1():
     collector = CsvDataCollector("./search_data.csv")
 
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function, search_space, n_iter=100, n_jobs=2)
+    hyper.add_search(
+        objective_function, search_space, n_iter=100, n_jobs=2, memory=False
+    )
     hyper.run()
 
     search_data1 = hyper.results(objective_function)
 
     collector.save(search_data1)
     search_data2 = collector.load()
+    collector.remove()
 
     search_data_equal(search_data1, search_data2)
 
@@ -57,72 +64,94 @@ def test_csv_2():
     collector = CsvDataCollector("./search_data.csv")
 
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function, search_space, n_iter=100, n_jobs=-1)
+    hyper.add_search(
+        objective_function, search_space, n_iter=100, n_jobs=-1, memory=False
+    )
     hyper.run()
 
     search_data1 = hyper.results(objective_function)
 
     collector.save(search_data1)
     search_data2 = collector.load()
+    collector.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-collector = CsvDataCollector("./search_data.csv")
+collector1 = CsvDataCollector("./search_data1.csv")
 
 
-def objective_function_append(para):
+def objective_function_append1(para):
     score = -para["x1"] * para["x1"]
 
     para_dict = para.para_dict
     para_dict["score"] = score
-    collector.append(para_dict)
+    collector1.append(para_dict)
 
     return score
 
 
-search_space = {
-    "x1": np.arange(0, 1000, 0.1),
-}
-
-
 def test_csv_3():
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function_append, search_space, n_iter=100)
+    hyper.add_search(objective_function_append1, search_space, n_iter=100, memory=False)
     hyper.run()
 
-    search_data1 = hyper.results(objective_function_append)
-
-    collector.save(search_data1)
-    search_data2 = collector.load()
+    search_data1 = hyper.results(objective_function_append1)
+    search_data2 = collector1.load()
+    collector1.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-collector = CsvDataCollector("./search_data.csv")
+collector2 = CsvDataCollector("./search_data2.csv")
+
+
+def objective_function_append2(para):
+    score = -para["x1"] * para["x1"]
+
+    para_dict = para.para_dict
+    para_dict["score"] = score
+    collector2.append(para_dict)
+
+    return score
 
 
 def test_csv_4():
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function_append, search_space, n_iter=100, n_jobs=4)
+    hyper.add_search(
+        objective_function_append2, search_space, n_iter=100, n_jobs=4, memory=False
+    )
     hyper.run()
 
-    search_data1 = hyper.results(objective_function_append)
-
-    collector.save(search_data1)
-    search_data2 = collector.load()
+    search_data1 = hyper.results(objective_function_append2)
+    search_data2 = collector2.load()
+    collector2.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
+collector3 = CsvDataCollector("./search_data3.csv")
+
+
+def objective_function_append3(para):
+    score = -para["x1"] * para["x1"]
+
+    para_dict = para.para_dict
+    para_dict["score"] = score
+    collector3.append(para_dict)
+
+    return score
+
+
 def test_csv_5():
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function_append, search_space, n_iter=100, n_jobs=-1)
+    hyper.add_search(
+        objective_function_append3, search_space, n_iter=100, n_jobs=-1, memory=False
+    )
     hyper.run()
 
-    search_data1 = hyper.results(objective_function_append)
-
-    collector.save(search_data1)
-    search_data2 = collector.load()
+    search_data1 = hyper.results(objective_function_append3)
+    search_data2 = collector3.load()
+    collector3.remove()
 
     search_data_equal(search_data1, search_data2)
--- tests/test_sql.py
+++ tests/test_sql.py
@@ -20,109 +20,138 @@ def search_data_equal(search_data1, search_data2):
     scores1 = search_data1["score"].values
     scores2 = search_data2["score"].values
 
+    print("\n scores1 \n", scores1, scores1.shape)
+    print("\n scores2 \n", scores2, scores2.shape)
+
     assert abs(np.sum(scores1) - np.sum(scores2)) < 0.001
 
 
-def test_csv_0():
+def test_sql_0():
     collector = SqlDataCollector("sqlite:///search_data.db")
 
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function, search_space, n_iter=100)
+    hyper.add_search(objective_function, search_space, n_iter=100, memory=False)
     hyper.run()
 
     search_data1 = hyper.results(objective_function)
 
     collector.save(search_data1)
     search_data2 = collector.load()
+    collector.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-def test_csv_1():
+def test_sql_1():
     collector = SqlDataCollector("sqlite:///search_data.db")
 
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function, search_space, n_iter=100, n_jobs=2)
+    hyper.add_search(
+        objective_function, search_space, n_iter=100, n_jobs=2, memory=False
+    )
     hyper.run()
 
     search_data1 = hyper.results(objective_function)
 
     collector.save(search_data1)
     search_data2 = collector.load()
+    collector.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-def test_csv_2():
+def test_sql_2():
     collector = SqlDataCollector("sqlite:///search_data.db")
 
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function, search_space, n_iter=100, n_jobs=-1)
+    hyper.add_search(
+        objective_function, search_space, n_iter=100, n_jobs=-1, memory=False
+    )
     hyper.run()
 
     search_data1 = hyper.results(objective_function)
 
     collector.save(search_data1)
     search_data2 = collector.load()
+    collector.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-collector = SqlDataCollector("sqlite:///search_data.db")
+collector1 = SqlDataCollector("sqlite:///search_data1.db")
 
 
-def objective_function_append(para):
+def objective_function_append1(para):
     score = -para["x1"] * para["x1"]
 
     para_dict = para.para_dict
     para_dict["score"] = score
-    collector.append(para_dict)
+    collector1.append(para_dict)
 
     return score
 
 
-search_space = {
-    "x1": np.arange(0, 1000, 0.1),
-}
-
-
-def test_csv_3():
+def test_sql_3():
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function_append, search_space, n_iter=100)
+    hyper.add_search(objective_function_append1, search_space, n_iter=100, memory=False)
     hyper.run()
 
-    search_data1 = hyper.results(objective_function_append)
-
-    collector.save(search_data1)
-    search_data2 = collector.load()
+    search_data1 = hyper.results(objective_function_append1)
+    search_data2 = collector1.load()
+    collector1.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-collector = SqlDataCollector("sqlite:///search_data.db")
+collector2 = SqlDataCollector("sqlite:///search_data2.db")
 
 
-def test_csv_4():
+def objective_function_append2(para):
+    score = -para["x1"] * para["x1"]
+
+    para_dict = para.para_dict
+    para_dict["score"] = score
+    collector2.append(para_dict)
+
+    return score
+
+
+def test_sql_4():
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function_append, search_space, n_iter=100, n_jobs=4)
+    hyper.add_search(
+        objective_function_append2, search_space, n_iter=100, n_jobs=4, memory=False
+    )
     hyper.run()
 
-    search_data1 = hyper.results(objective_function_append)
-
-    collector.save(search_data1)
-    search_data2 = collector.load()
+    search_data1 = hyper.results(objective_function_append2)
+    search_data2 = collector2.load()
+    collector2.remove()
 
     search_data_equal(search_data1, search_data2)
 
 
-def test_csv_5():
+collector3 = SqlDataCollector("sqlite:///search_data3.db")
+
+
+def objective_function_append3(para):
+    score = -para["x1"] * para["x1"]
+
+    para_dict = para.para_dict
+    para_dict["score"] = score
+    collector3.append(para_dict)
+
+    return score
+
+
+def test_sql_5():
     hyper = Hyperactive(verbosity=False)
-    hyper.add_search(objective_function_append, search_space, n_iter=100, n_jobs=-1)
+    hyper.add_search(
+        objective_function_append3, search_space, n_iter=100, n_jobs=-1, memory=False
+    )
     hyper.run()
 
-    search_data1 = hyper.results(objective_function_append)
-
-    collector.save(search_data1)
-    search_data2 = collector.load()
+    search_data1 = hyper.results(objective_function_append3)
+    search_data2 = collector3.load()
+    collector3.remove()
 
     search_data_equal(search_data1, search_data2)
