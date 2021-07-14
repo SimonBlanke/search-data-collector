be082149d481f35e66172d6c91f13d0e3bed7fe1
Simon Blanke <simon.blanke@yahoo.com>
2021-07-14 16:06:24 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+import re
+
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = ["pandas", "sqlalchemy", "filelock"]
+
+
+def find_version(*filepath):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, *filepath)) as fp:
+        version_match = re.search(
+            r"^__version__ = ['\"]([^'\"]*)['\"]", fp.read(), re.M
+        )
+        if version_match:
+            return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="hyperactive-data-collector",
+    version=find_version("hyperactive_data_collector/__init__.py"),
+    author="Simon Blanke",
+    author_email="simon.blanke@yahoo.com",
+    license="MIT",
+    description="Thread safe data collection into csv-files or sql-databases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["data-science"],
+    url="https://github.com/SimonBlanke/data-collector",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+    ],
+    install_requires=requires,
+)
