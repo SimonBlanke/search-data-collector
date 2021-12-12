a7c1ce7d38b88f6d4c2334de76eba35e9769da80
Simon Blanke <simon.blanke@yahoo.com>
2021-12-12 20:09:57 +0100

    remove unused code

--- hyperactive_data_storage/search_data_converter.py
+++ hyperactive_data_storage/search_data_converter.py
@@ -7,36 +7,6 @@ import numbers
 import numpy as np
 
 
-def init_data_types(search_space):
-    data_types = {}
-    for para_name in search_space.keys():
-        value0 = search_space[para_name][0]
-
-        if isinstance(value0, numbers.Number):
-            type0 = "number"
-            search_dim_ltm = search_space[para_name]
-
-        elif isinstance(value0, str):
-            type0 = "string"
-            search_dim_ltm = search_space[para_name]
-
-        elif callable(value0):
-            type0 = "function"
-
-            search_dim_ltm = []
-            for func in list(search_space[para_name]):
-                search_dim_ltm.append(func.__name__)
-        else:
-            type0 = None
-            search_dim_ltm = search_space[para_name]
-            print("Warning! data type of ", para_name, " not recognized")
-            print("Memory will not work")
-
-        data_types[para_name] = type0
-
-    return data_types
-
-
 class SearchDataConverter:
     def __init__(self):
         self.data_types = None
