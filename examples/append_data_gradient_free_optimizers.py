20dd37c9d573c605fe0b076c3083334476ae83c4
Simon Blanke <simon.blanke@yahoo.com>
2024-04-18 14:12:31 +0200

    separate example into hyperactive and gfo

--- examples/append_data_gradient_free_optimizers.py
+++ examples/append_data_gradient_free_optimizers.py
@@ -1,8 +1,9 @@
 import numpy as np
-from hyperactive import Hyperactive
+from gradient_free_optimizers import RandomSearchOptimizer
 from search_data_collector import CsvSearchData
 
 collector = CsvSearchData("./search_data.csv")  # the csv is created automatically
+collector.remove()  # cleanup existing data
 
 
 def parabola_function(para):
@@ -16,16 +17,14 @@ def parabola_function(para):
 
 
 search_space = {
-    "x": list(np.arange(-10, 10, 0.1)),
-    "y": list(np.arange(-10, 10, 0.1)),
+    "x": np.arange(-10, 10, 0.1),
+    "y": np.arange(-10, 10, 0.1),
 }
 
 
-hyper = Hyperactive()
-hyper.add_search(parabola_function, search_space, n_iter=1000)
-hyper.run()
-search_data = hyper.search_data(parabola_function)
+opt = RandomSearchOptimizer(search_space)
+opt.search(parabola_function, n_iter=100)
+search_data = opt.search_data
 
 search_data = collector.load(search_space)  # load data
-
 print("\n search_data \n", search_data)
